5282ce7be405789b57a33d8755a7ecfd11eacd2c
Rasel Rana <rasel@artificial-soft.com>
2022-02-17 18:21:23 +0600

    email

--- project/settings.py
+++ project/settings.py
@@ -124,17 +124,19 @@ STATIC_URL = 'static/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
+#For email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'developer1000486@gmail.com'
+EMAIL_HOST_PASSWORD = 'developer@486'
+EMAIL_PORT = 587
+AIL_USE_TLS = True
+DEFAULT_FROM_EMAIL ='OTF Team <develper1000486@gmail.com>'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#For email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'raselrana1000486@gmail.com'
-EMAIL_HOST_PASSWORD = 'raselrana1147'
-EMAIL_PORT = 587
-AIL_USE_TLS = True
-DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
